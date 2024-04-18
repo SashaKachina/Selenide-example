ae68fee1e4a306cade20fbb0edfdc7538f577041
Konstantin Zhukov <k.zhukov@vk.team>
2024-04-19 00:14:24 +0300

    QA-666: add new step

--- src/test/java/com/qa/LoginFormTest.java
+++ src/test/java/com/qa/LoginFormTest.java
@@ -6,6 +6,7 @@ import static com.codeborne.selenide.Condition.visible;
 
 public class LoginFormTest extends BaseTest {
 
+
     @Test(
             description = "Проверка открытия формы логина с главной страницы при нажатии на 'Войти'",
             testName = "Открытие формы логина с главной"
